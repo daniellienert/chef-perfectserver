bac35d9b9653b680446d57c1b59399713d3bee16
Daniel Lienert <daniel@lienert.cc>
2013-05-05 13:40:24 +0200

    TASK :renamed server cookbook to perfectserver cookbook

--- site-cookbooks/server/attributes/default.rb
+++
@@ -1,8 +0,0 @@
-## Additional Cookbook attributes
-
-# phpmyadmin
-default['phpmyadmin']['server_name'] = node[:fqdn]
-default['phpmyadmin']['server_aliases'] = ['phpmyadmin.*.*']
-
-# roundcube
-default['roundcube']['host'] = node[:fqdn]
