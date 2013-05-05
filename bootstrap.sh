4115a1eb38c0023c9b19a915fa5b1e1161341a01
Daniel Lienert <daniel@lienert.cc>
2013-05-05 15:43:19 +0200

    FIX: solo.rb

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt-get -q -y install ruby-dev
+apt-get -q -y install build-essential ruby-dev
 gem install chef --version 10.24.4 --no-rdoc --no-ri --conservative
--- solo.rb
+++ solo.rb
@@ -1,5 +1,5 @@
 root = File.absolute_path(File.dirname(__FILE__))
 
 file_cache_path root
-cookbook_path [root + "cookbooks", root + "site-cookbooks"]
-roles_path = root + "roles"
+cookbook_path [root + "/cookbooks", root + "/site-cookbooks"]
+roles_path = root + "/roles"
